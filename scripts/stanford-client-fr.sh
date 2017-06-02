b4f540759b312f68bc72aa25da480f469a7ebcf1
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2017-06-02 13:19:00 +0200

    Additional stanford-related scripts

--- scripts/stanford-client-fr.sh
+++ scripts/stanford-client-fr.sh
@@ -8,6 +8,12 @@ fi
 
 input=$1
 
+# Properties
+
+# props=
+
 # Request
 
-wget --post-data "$1" 'localhost:9000/?properties={"annotators":"tokenize,ssplit,pos,parse","parse.model":"edu/stanford/nlp/models/lexparser/frenchFactored.ser.gz","pos.model":"edu/stanford/nlp/models/pos-tagger/french/french.tagger","tokenize.language":"fr","outputFormat":"json"}' -O -
+# wget --post-data "$1" 'localhost:9000/?properties={"annotators":"tokenize,ssplit,pos,parse","parse.model":"edu/stanford/nlp/models/lexparser/frenchFactored.ser.gz","pos.model":"edu/stanford/nlp/models/pos-tagger/french/french.tagger","tokenize.language":"fr","outputFormat":"json"}' -O -
+
+curl --data "$1" 'localhost:9000/?properties=%7B%22annotators%22%3A%22tokenize%2Cssplit%2Cpos%2Cparse%22%2C%22parse.model%22%3A%22edu%2Fstanford%2Fnlp%2Fmodels%2Flexparser%2FfrenchFactored.ser.gz%22%2C%22pos.model%22%3A%22edu%2Fstanford%2Fnlp%2Fmodels%2Fpos-tagger%2Ffrench%2Ffrench.tagger%22%2C%22tokenize.language%22%3A%22fr%22%2C%22outputFormat%22%3A%22json%22%7D' -o -
---
+++ scripts/stanford-train-fr.sh
@@ -0,0 +1,36 @@
+# Config
+
+scriptdir=/home/kuba/work/odil/third-party/stanford/tool/stanford-parser-full-2016-10-31
+memory=4000m
+
+
+# Params
+
+if [ ! $# -eq 3 ]; then
+  echo Usage: `basename $0` 'TRAIN' 'TEST' 'OUT'
+  echo
+  echo TRAIN: a file with syntax trees in the PTB format used for training
+  echo TEST: a file with syntax trees in the PTB format used for testing 
+  echo OUT: output model
+  echo
+  exit
+fi
+
+trainPath=$1
+testPath=$2
+outModel=$3
+
+
+# Training
+
+# java -mx"$memory" -cp "$scriptdir/*:" edu.stanford.nlp.parser.lexparser.LexicalizedParser \
+#   -tLPP edu.stanford.nlp.parser.lexparser.FrenchTreebankParserParams \
+#   -train "$trainPath" \
+#   -testTreebank "$testPath" \
+
+java -mx"$memory" -cp "$scriptdir/*:" edu.stanford.nlp.parser.lexparser.LexicalizedParser \
+  -tLPP edu.stanford.nlp.parser.lexparser.FrenchTreebankParserParams \
+  -encoding utf-8 \
+  -train "$trainPath" \
+  -testTreebank "$testPath" \
+  -saveToSerializedFile "$outModel"
