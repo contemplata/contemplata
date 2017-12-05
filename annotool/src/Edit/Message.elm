6ffca75aa52a7a3fb41d821baa537357e902fd8e
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2017-12-05 17:23:09 +0100

    Remove bug

--- annotool/src/Edit/Message.elm
+++ annotool/src/Edit/Message.elm
@@ -472,10 +472,11 @@ update msg model =
     SwapFileTo fid -> idle <| M.moveToFirst model.focus fid model
 
     Compare -> idle <|
-        let topId = M.getReprId M.Top (M.selectWin M.Top model).tree model
-            topTree = M.getTree M.Top topId model
-            botId = M.getReprId M.Bot (M.selectWin M.Bot model).tree model
-            botTree = M.getTree M.Bot topId model
+        let getTree foc =
+                let id = M.getReprId foc (M.selectWin foc model).tree model
+                in M.getTree foc id model
+            topTree = getTree M.Top
+            botTree = getTree M.Bot
             (topIds, botIds) = Compare.compareSyntax topTree botTree
         in  M.setSelection (S.fromList topIds) M.Top <|
             M.setSelection (S.fromList botIds) M.Bot <|
