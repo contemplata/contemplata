124f7361d2fd57d587bec3411e04114854b70b29
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2018-01-18 16:24:26 +0100

    Finish cleaning-up the main types-related module

--- backend/src/Contemplata/Types.hs
+++ backend/src/Contemplata/Types.hs
@@ -3,7 +3,9 @@
 {-# LANGUAGE RecordWildCards #-}
 
 
--- | Types of the annotation server.
+-- | The main Contemplata types: `File`, `Token`, `Sent`, `Turn`, etc.
+--
+-- Re-exports `Contemplata.Types.Core`.
 
 
 module Contemplata.Types
@@ -27,22 +29,20 @@ module Contemplata.Types
 , defaultMeta
 
 -- * Types
-, Sent
+-- ** Sentence
 , Token (..)
+, Sent
+, sentFromText
+-- ** Tree
 , Tree
 , Node (..)
+-- ** Link
+, Link (..)
+-- ** File
 , File (..)
 , Turn (..)
-, Link (..)
--- , LinkData (..)
-
--- * Utils
 , mkNewFile
-, sentFromText
 , numberOfTokens
--- , emptyTok
--- , mergeToks
--- , concatToks
 ) where
 
 
@@ -60,21 +60,9 @@ import qualified Contemplata.Anno.Entity as E
 import Contemplata.Types.Core
 
 
------------
--- Types
------------
-
-
--- | A sentence underlying a syntactic tree.
--- type Sent = T.Text
-type Sent = [Token]
-
-
--- | Create a sentence from text.
-sentFromText :: T.Text -> Sent
-sentFromText =
-  let mkTok x = Token {orth=x, afterSpace=True}
-  in  map mkTok . T.words
+----------------------
+-- Sentence
+----------------------
 
 
 -- | Sentence token
@@ -82,11 +70,11 @@ data Token = Token
   { orth :: T.Text
     -- ^ Orthographic form (original, as in the source file)
   , afterSpace :: Bool
-    -- ^ Is it after space?  NOTE: should be true by default.
+    -- ^ Is it after space?
   } deriving (Generic, Show, Eq, Ord)
 
 
--- | Token concatenation.
+-- | An empty token.
 emptyTok :: Token
 emptyTok = Token {orth = "", afterSpace = False}
 
@@ -103,16 +91,32 @@ mergeToks x y = Token
       else ""
 
 
--- -- | Token concatenation.
--- concatToks :: [Token] -> Token
--- concatToks =
---   let strip tok = tok {orth = T.strip (orth tok)}
---   in  strip . foldl' mergeToks emptyTok
-
-
 instance Monoid Token where
   mempty = emptyTok
   mappend = mergeToks
+
+
+----------------------
+-- Sentence
+----------------------
+
+
+-- | A sentence underlying a syntactic tree. Note that the syntactic tree is not
+-- required to use all the tokens in the underlying sentence (see also
+-- `leafPos`).
+type Sent = [Token]
+
+
+-- | Create a sentence from text, using whitespaces as token delimiters.
+sentFromText :: T.Text -> Sent
+sentFromText =
+  let mkTok x = Token {orth=x, afterSpace=True}
+  in  map mkTok . T.words
+
+
+----------------------
+-- Sentence
+----------------------
 
 
 -- | A syntactic tree.
@@ -123,55 +127,97 @@ type Tree = R.Tree Node
 data Node
   = Node
     { nodeId :: NodeId
+      -- ^ Each node in the tree must have a unique ID
     , nodeVal :: T.Text
+      -- ^ A textual value assigned to the node
     , nodeTyp :: Maybe E.Entity
-    , nodeComment :: T.Text }
+      -- ^ Node's type or, alternatively, the corresponding annotation (if any)
+    , nodeComment :: T.Text
+      -- ^ Annotator's comment, completely optional
+    }
   | Leaf
     { leafId :: NodeId
+      -- ^ Leaf's equivalent of `nodeId`. The sets of `leafId`s and `nodeId`s
+      -- should be disjoint.
     , leafVal :: T.Text
-      -- ^ NOTE: This can be different than in the corresponding sentence token,
-      -- because we preprocess tokens for the sake of parsing!
+      -- ^ A value assigned to the leaf. NOTE: It can be different than in the
+      -- corresponding sentence token, because we preprocess tokens for the sake
+      -- of parsing.
     , leafPos :: Int
-    , leafComment :: T.Text }
+      -- ^ Position of the leaf in the underlying `Sent`ence. This is needed
+      -- because (a) not every token is required to be referenced from the tree,
+      -- (b) this allows to represent non-projective trees.
+    , leafComment :: T.Text
+      -- ^ An equivalent of `nodeComment`
+    }
   deriving (Generic, Show, Eq, Ord)
+
+
+----------------------
+-- Link
+----------------------
+
+
+-- | A link between two nodes in a given file.
+data Link = Link
+  { from :: Addr
+    -- ^ Starting point
+  , to :: Addr
+    -- ^ Ending point
+  } deriving (Generic, Show, Eq, Ord)
+
+
+----------------------
+-- File
+----------------------
 
 
 -- | A file.
 data File = File
-  { treeMap :: M.Map TreeId Tree
-    -- ^ The annotated trees
-    --
-    -- IMPORTANT: the trees are assigned only to partition representatives (see
-    -- `partMap`).
-    --
-    -- UPDATE 23/10/2017: We assume that `TreeId`s determine the order of the
-    -- trees in the turn (i.e. the trees with lower id's are first).
-    --
-    -- UPDATE 23/10/2017: We change the type of the `treeMap` from:
-    --   :: M.Map TreeId (Sent, Tree)
-    -- to:
-    --   :: M.Map TreeId Tree,
-    -- where information about sentences is now preserved in `sentMap`.
+  { turns :: [Turn]
+    -- ^ The list of turns in the file. In ANCOR files, there is also a division
+    -- on episodes and sections. We don't preserve it here, though, assuming
+    -- that it can be easily retrieved.
 
   , sentMap :: M.Map TreeId Sent
-    -- ^ The corresponding (raw) sentences (NEW 23/10/2017)
+    -- ^ The sentences corresponding to the individual `TreeId`s in the
+    -- individual `Turn`s.
 
   , partMap :: M.Map TreeId (S.Set TreeId)
-    -- ^ Tree partitions, which groups the trees in sets of trees (NEW
-    -- 23/10/2017).
-    --
-    -- UPDATE 01/11/2017: trees are assigned only to partition representatives.
+    -- ^ Tree partitions group sentences into sets of sentences. The trees are
+    -- then only assigned to the entire partitions.
 
   , reprMap :: M.Map TreeId TreeId
-    -- ^ Each ID points to its direct representative.
+    -- ^ Each ID points to its direct representative. This map thus allows to
+    -- obtain the Id of the partition to which a given sentence/tree ID belongs,
+    -- by subsequently following the pointers.
+    --
+    -- TODO: this should be probably removed from the model, at least on the
+    -- back-end side, as it leads to information redundancy (all we need is
+    -- already in the `partMap`).
 
-  , turns :: [Turn]
-    -- ^ The list of turns in the file (we don't preserve the division on
-    -- episodes and sections)
+  , treeMap :: M.Map TreeId Tree
+    -- ^ A set of `Tree`s in the file, together with their unique `TreeId`s,
+    -- assigned to the individual partitions (see `partMap`).
+    --
+    -- It is imprortant to keep in mind that:
+    -- * The trees are assigned only to partition representatives (see
+    --   `partMap`).
+    -- * `TreeId`s determine the order of the trees in the turn (i.e. the trees
+    --   with lower id's are first).
 
-  -- , linkMap :: M.Map Link LinkData
   , linkMap :: M.Map Link E.Entity
+    -- ^ The set of links, together with the corresponding annotations
+  } deriving (Generic, Show, Eq)
 
+
+-- | A turn (related to `Contemplata.Ancor.Types.Turn`) can contain several
+-- utterances (and, hence, trees), and for each tree we *might* know who is the
+-- author of this utterance (speaker ID, i.e., its position in the `speaker`
+-- list). We also *might* know (`speaker`) the list of speakers.
+data Turn = Turn
+  { speaker :: [T.Text]
+  , trees :: M.Map TreeId (Maybe Int)
   } deriving (Generic, Show, Eq)
 
 
@@ -198,29 +244,6 @@ mkNewFile treeMap0 turns =
       $ treeMap0
 
 
--- | A turn (related to `Ancor.Turn`) can contain several utterances (and,
--- hence, trees), and for each tree we *might* know who is the author of this
--- utterance (speaker ID, i.e., its position in the `speaker` list). We also
--- *might* know (`speaker`) the list of speakers.
-data Turn = Turn
-  { speaker :: [T.Text]
-  , trees :: M.Map TreeId (Maybe Int)
-  } deriving (Generic, Show, Eq)
-
-
--- | A link between two nodes in a given file.
-data Link = Link
-  { from :: Addr
-  , to :: Addr }
-  deriving (Generic, Show, Eq, Ord)
-
-
--- -- | Additional data assigned to a link.
--- data LinkData = LinkData
---   { signalAddr :: Maybe Addr
---   } deriving (Generic, Show, Eq)
-
-
 -----------
 -- Various
 -----------
@@ -239,10 +262,6 @@ numberOfTokens File{..} = sum
 -----------
 
 
--- instance JSON.FromJSON E.Entity
--- instance JSON.ToJSON E.Entity where
---   toEncoding = JSON.genericToEncoding JSON.defaultOptions
-
 instance JSON.FromJSON Node
 instance JSON.ToJSON Node where
   toEncoding = JSON.genericToEncoding JSON.defaultOptions
@@ -252,10 +271,6 @@ instance JSON.ToJSON Link where
   toEncoding = JSON.genericToEncoding JSON.defaultOptions
 instance JSON.FromJSONKey Link
 instance JSON.ToJSONKey Link
-
--- instance JSON.FromJSON LinkData
--- instance JSON.ToJSON LinkData where
---   toEncoding = JSON.genericToEncoding JSON.defaultOptions
 
 instance JSON.FromJSON Token
 instance JSON.ToJSON Token where
